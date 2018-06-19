7cc4de697bd5249af8155c8cc6f88d91deae08e8
Mohamad171 <mohamadmohamadi249@gmail.com>
2018-06-19 11:41:43 +0430

    Add footballpredict directory

---
+++ footballpredict/beakerfile.py
@@ -0,0 +1,15 @@
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+
+cache_opts = {
+    'cache.type': 'file',
+    'cache.data_dir': 'cache/data',
+    'cache.lock_dir': 'cache/lock',
+    'cache.regions': 'short_term, long_term',
+    'cache.short_term.type': 'ext:memcached',
+    'cache.short_term.url': '127.0.0.1.11211',
+    'cache.short_term.expire': '3600',
+    'cache.long_term.type': 'file',
+    'cache.long_term.expire': '86400',
+}
+cache = CacheManager(**parse_cache_config_options(cache_opts))
